b418dc40108bbb34d4dfca7387d3c6ac6b64087a
Yume <92096632+Natcharika@users.noreply.github.com>
2024-03-03 22:49:22 +0700

    CSB01-3

--- src/CSB01.jsx
+++ src/CSB01.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import { useState } from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Grid from '@mui/system/Unstable_Grid/Grid';
@@ -9,9 +9,15 @@ import MenuItem from '@mui/material/MenuItem';
 import FormHelperText from '@mui/material/FormHelperText';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormLabel from '@mui/material/FormLabel';
+import Checkbox from '@mui/material/Checkbox';
+import FormGroup from '@mui/material/FormGroup';
 
 function FormPropsTextFields() {
-    const [StudentID, setStudentID] = React.useState('');
+    const [StudentID, setStudentID] = useState('');
 
     const handleChange = (event) => {
         setStudentID(event.target.value);
@@ -48,7 +54,7 @@ function FormPropsTextFields() {
                     sx={{
                         marginTop: 5,
                         marginLeft: 50,
-                        
+
                     }}>
                     รหัสนักศึกษา คนที่ 1
                     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
@@ -86,7 +92,7 @@ function FormPropsTextFields() {
                         <TextField
                             disabled
                             id="Name"
-                            label="Name"
+                            label="ชื่อ - สกุล"
                             defaultValue="ชื่อ - สกุล" //ชื่อจะดึงข้อมูลมาจากรหัสนักศึกษา
                         />
                     </Box>
@@ -96,7 +102,7 @@ function FormPropsTextFields() {
                     sx={{
                         marginTop: 5,
                         marginLeft: 50,
-                        
+
                     }}>
                     รหัสนักศึกษา คนที่ 2
                     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
@@ -134,11 +140,117 @@ function FormPropsTextFields() {
                         <TextField
                             disabled
                             id="Name"
-                            label="Name"
+                            label="ชื่อ - สกุล"
                             defaultValue="ชื่อ - สกุล" //ชื่อจะดึงข้อมูลมาจากรหัสนักศึกษา
                         />
                     </Box>
                 </Typography>
+            </div>
+            <div>
+                <Typography
+                    fontSize='18px'
+                    sx={{
+                        marginTop: 5,
+                        marginLeft: 50,
+
+                    }}>
+                    ชื่อโครงงานภาษาอังกฤษ
+                    <Box
+                        sx={{
+                            '& .MuiTextField-root': { width: '75ch' },
+                            marginTop: 1,
+                            marginLeft: 3,
+                            display: "flex",
+                        }}
+                    >
+                        <TextField
+                            required
+                            id="Project-name"
+                            label="ชิ่อโครงงานภาษาอังกฤษ"
+                            defaultValue="Project name"
+                        />
+                    </Box>
+                </Typography>
+            </div>
+            <div>
+                <Typography
+                    fontSize='18px'
+                    sx={{
+                        marginTop: 5,
+                        marginLeft: 50,
+                    }}
+                >
+                    มีอาจารย์ที่ปรึกษาหรือไม่ ?
+                </Typography>
+                <FormControl
+                    fontSize='18px'
+                    sx={{
+                        marginTop: 0,
+                        marginLeft: 55,
+                    }}
+                >
+                    <RadioGroup
+                        row
+                        aria-labelledby="demo-row-radio-buttons-group-label"
+                        name="row-radio-buttons-group"
+                    >
+                        <FormControlLabel value="Yes" control={<Radio />} label="มี" />
+                        <FormControlLabel value="No" control={<Radio />} label="ไม่มี" sx={{ marginLeft: 10 }} />
+                    </RadioGroup>
+                </FormControl>
+            </div>
+            <div>
+                <Typography
+                    fontSize='18px'
+                    sx={{
+                        marginTop: 5,
+                        marginLeft: 50,
+                    }}
+                >
+                    โครงงานนี้จัดอยู่ในประเภท
+                </Typography>
+                <FormGroup
+                    fontSize='18px'
+                    sx={{
+                        marginTop: 0,
+                        marginLeft: 55,
+                    }}
+                >
+                    <FormControlLabel control={<Checkbox />} label="Networks & Data Communications" />
+                    <FormControlLabel control={<Checkbox />} label="Graphics & Animation" />
+                    <FormControlLabel control={<Checkbox />} label="Database & Web" />
+                    <FormControlLabel control={<Checkbox />} label="Artificial Intelligence" />
+                    <FormControlLabel control={<Checkbox />} label="Games" />
+                    <FormControlLabel control={<Checkbox />} label="Other" />
+                </FormGroup>
+            </div>
+            <div>
+                <Typography
+                    fontSize='18px'
+                    sx={{
+                        marginTop: 5,
+                        marginLeft: 50,
+                    }}
+                >
+                    รายละเอียดของโครงงานที่จะพัฒนา
+                </Typography>
+                <Box
+                    sx={{
+                        '& .MuiTextField-root': { width: '85ch' },
+                    }}
+                >
+                    <TextField
+                        fontSize='18px'
+                        sx={{
+                            marginTop: 0,
+                            marginLeft: 55,
+                        }}
+                        id="outlined-multiline-flexible"
+                        label="Multiline"
+                        multiline
+                        maxRows={8}
+                    />
+                </Box>
             </div>
         </Box>
     );
