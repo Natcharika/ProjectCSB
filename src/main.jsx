f23d530e6bd86206ac80cc83cfb6e45865d999e9
Songthum Kongsomprach <yos2544@gmail.com>
2024-03-04 00:07:29 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import SignIn from './login.jsx'
-import './login.jsx'
-import FormPropsTextFields from './CSB01.jsx'
-import './CSB01.jsx'
+// import SignIn from './login.jsx'
+// import './login.jsx'
+// import FormPropsTextFields from './CSB01.jsx'
+// import './CSB01.jsx'
+// import AverageScoreCalculator from './component/MyComponent.jsx'
+// import MultipleSelectChip from './component/as.jsx'
+// import MyComponent from './component/UplinkS2.jsx'
+// import TestSpecial2 from './component/S2.jsx'
+// import TestSpecial1 from './component/S1.jsx'
+// import LinkS2 from './component/UplinkS2.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <SignIn /> */}
+    {/* <SignIn />
     <FormPropsTextFields/>
+    <AverageScoreCalculator/>
+    <MultipleSelectChip/> */}
+    {/* <MyComponent/> */}
+    {/* <TestSpecial2/>
+    <TestSpecial1/>
+    <LinkS2/> */}
   </React.StrictMode>,
 )
